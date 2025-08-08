1e835175c64b7b773d3ef92da8f3ab822ef3166e
akhilachikati <akhilachikati111@gmail.com>
2025-08-08 19:30:12 +0530

    first commit

---
+++ pwgen.py
@@ -0,0 +1,8 @@
+from werkzeug.security import generate_password_hash
+print(generate_password_hash('admin123'))
+print(generate_password_hash('admin234'))
+print(generate_password_hash('admin345'))
+print(generate_password_hash('admin456'))
+print(generate_password_hash('admin567'))
+print(generate_password_hash('admin678'))
+print(generate_password_hash('admin789'))
